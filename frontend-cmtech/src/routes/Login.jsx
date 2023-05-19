e76985d8e74398b280da480d73a90d0b263fa76e
larialbu <larissaamca@gmail.com>
2023-05-19 10:49:27 -0300

    correção fornt end

--- frontend-cmtech/src/routes/Login.jsx
+++ frontend-cmtech/src/routes/Login.jsx
@@ -1,6 +1,5 @@
-import './Login.css'
-
-import React, { useState,useContext,AuthContext } from "react";
+import './Login.css';
+import React, { useState} from "react";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { TextField } from '@mui/material';
@@ -10,38 +9,13 @@ import { api } from '../libs/Api';
 
 const Login = () => {
 
-    /* const [email, setEmail] = useState("")
+    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
  
     async function login() {
         await api.post("Login", {email: email, password: password}).then(response => {
             console.log(response.data)
         })
-    } */
-
-    const auth = useContext(AuthContext);
-    const navigate = useNavigate();
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleEmailInput = (event) => {
-        setEmail(event.target.value);
-    }
-
-    const handlePasswordInput = (event) => {
-        setPassword(event.target.value);
-    }
-
-    const handleLogin = async () => {
-        if (email && password) {
-            const isLogged = await auth.signin(email, password);
-            if (isLogged) {
-                navigate('/');
-            } else {
-                alert("Não deu certo.");
-            }
-        }
     }
 
     return(
@@ -52,17 +26,17 @@ const Login = () => {
                     <TextField
                         className='input' id="email" label="E-mail" type="email" variant="outlined" fullWidth
                         value={email} sx={{marginTop: 4}}
-                        onChange={handleEmailInput/* e => setEmail(e.target.value) */}
+                        onChange={ e => setEmail(e.target.value)}
                     />
                     <TextField
                         className='input'id="senha" label="Senha" variant="outlined" margin="normal" fullWidth
                         value={password} sx={{marginTop: 4}} type="password"
-                        onChange={handlePasswordInput/* e => setPassword(e.target.value) */}
+                        onChange={e => setPassword(e.target.value)}
                     />                    
                     <Link to="/EsqueciSenha" className='Sublink'><p>Esqueci minha senha</p></Link>
                     <Button
                         variant='contained' color='secondary' sx={{background: '#4B0054', marginTop: 4,width:395}}
-                        onClick={handleLogin/* login */}
+                        onClick={login}
                         >
                             Entrar
                     </Button>
